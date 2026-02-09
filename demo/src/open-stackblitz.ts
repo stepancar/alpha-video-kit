6570a2d9b7d06373b5d6f0013cce74308b3cf656
Stepan Mikhailiuk <istepancar@gmail.com>
2026-02-10 03:41:45 +0800

    fix(*): fix

--- demo/src/open-stackblitz.ts
+++ demo/src/open-stackblitz.ts
@@ -1,10 +1,5 @@
-import sdk from '@stackblitz/sdk';
-
-const REPO = 'stepancar/alpha-video-kit/tree/main/examples';
+const BASE = 'https://stackblitz.com/github/stepancar/alpha-video-kit/tree/main/examples';
 
 export function openStackblitz(exampleDir: string, openFile: string = 'main.ts') {
-  sdk.openGithubProject(`${REPO}/${exampleDir}`, {
-    openFile: openFile,
-    newWindow: true,
-  });
+  window.open(`${BASE}/${exampleDir}?file=${openFile}`, '_blank');
 }
